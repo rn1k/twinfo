76af3073275de5c275dbd4f226cb5761a81df01f
rn1k <maki_r12@yahoo.co.jp>
2017-09-10 20:20:12 +0900

    for multiprocessing

--- src/Streaming.py
+++ src/Streaming.py
@@ -1,5 +1,30 @@
 import tweepy
 import sqlite3
 
+class App:
+    """docstring for App."""
+    def __init__(self, consumerKey, consumerSecret):
+        self.consumerKey = consumerKey
+        self.consumerSecret = consumerSecret
+
+
+class User:
+    """docstring for account."""
+    def __init__(self, App, accessToken, accessSecret):
+        self.App = App
+        self.consumerKey = App.consumerKey
+        self.consumerSecret = App.consumerSecret
+        self.accessToken = accessToken
+        self.accessSecret = accessSecret
+
+
 class Streaming:
-    pass
+    """各アカウントでのストリーミング処理"""
+    def __init__(self, User):
+        self.User = User
+        # TODO tweepyの認証
+
+    @classmethod
+    def start(self):
+        pass
+        #TODO 各ユーザの処理
--- src/main.py
+++ src/main.py
@@ -1,16 +1,27 @@
 import joblib
+import yaml
 import time
+from Streaming import App, User, Streaming
 
-def streaming(base):
-    # 結構時間かかる処理とする
-    print("sleep{}".format(base))
-    time.sleep(base/10000)
-    print([base+i for i in range(10)])
+def start_stream(Stream):
+    Stream.start()
 
+def get_users():
+    import pprint
+    YAML_PATH = 'src/conf/conf.yaml'
+    with open(YAML_PATH, 'r') as f:
+        data=yaml.load(f)
+    app = App(**data['app'])
+    settings = [setting for user, setting in data['users'].items()]
+    users= [User(app, **setting) for setting in settings]
+    return users
 
 def main():
-    pass
-    # results = joblib.Parallel(n_jobs=-1)([joblib.delayed(heavy_proc)(x) for x in range(1, int(5e4), 10000)])
+
+    users = get_users()
+    joblib.Parallel(n_jobs=len(users))(
+        [joblib.delayed(start_stream)(Streaming(user)) for user in users]
+    )
 
 
 if __name__ == '__main__':
